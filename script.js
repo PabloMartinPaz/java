0a65ba8a97e20378f535bae2ab48e71ef1e64404
PabloMartinPaz <100540211+PabloMartinPaz@users.noreply.github.com>
2022-05-04 21:37:43 -0300

    ciclo2

--- script.js
+++ script.js
@@ -1,33 +1,46 @@
-const pesocajonvacio = 0.8;
-const gananciafinal = 1.4;
-const gananciareventa = 1.2;
-let pesocajon  ;
-let preciocajon ;
+const pesoCajonVacio = 0.8;
+const gananciaFinal = 1.4;
+const gananciaReventa = 1.2;
+let pesoCajon  ;
+let precioCajon ;
 let producto;
-let costokg;
-let precioventa
+let costoKg;
+let precioVenta
+let id;
+const final = (pesoCajon,precioCajon) =>  (precioCajon / (pesoCajon - pesoCajonVacio) * gananciaFinal);
+const reventa = (pesoCajon,precioCajon) =>  (precioCajon / (pesoCajon - pesoCajonVacio) * gananciaReventa);
+class productos{
+    constructor(id, producto, pesoCajon, precioCajon, precioReventa, precioVenta){
+        this.id = id;
+        this.producto = producto
+        this.pesoCajon = pesoCajon
+        this.precioCajon = precioCajon
+        this.precioReventa = precioReventa = reventa
+        this.precioVenta = precioVenta = final
 
-const final = (pesocajon,preciocajon) =>  (preciocajon / (pesocajon - pesocajonvacio) * gananciafinal);
-const reventa = (pesocajon,preciocajon) =>  (preciocajon / (pesocajon - pesocajonvacio) * gananciareventa);
-
+    }
+}
+const productosGuardados= [];
 
 do {
+    productosGuardados.push (new productos())
+    id = prompt ("ingrese id del producto")
     producto = prompt ("ingrese nombre del producto")
-    tipocliente = prompt ("ingrese tipo de cliente");
+    tipoCliente = prompt ("ingrese tipo de cliente");
 
-    pesocajon = parseFloat(prompt ("ingrese peso total del cajon"));
-    preciocajon = parseFloat(prompt ("ingrese el costo del cajon"));
+    pesoCajon = parseFloat(prompt ("ingrese peso total del cajon"));
+    precioCajon = parseFloat(prompt ("ingrese el costo del cajon"));
     
-    if ( isNaN (pesocajon) || isNaN (preciocajon) ){
+    if ( isNaN (pesoCajon) || isNaN (precioCajon) ){
         alert ("ingrese un valor valido");
     }
-} while( isNaN(pesocajon) || isNaN(preciocajon) )
- switch (tipocliente) {
+} while( isNaN(pesoCajon) || isNaN(precioCajon) )
+ switch (tipoCliente) {
      case "final":
-         alert ("el precio del kg de " + producto + " es $" + final(pesocajon,preciocajon));
+         alert (`el precio del kg de ${producto}  es $ ${final(pesoCajon, precioCajon)}`);
          break
     case "reventa":
-            alert ("el precio del kg de " + producto + " es $" + reventa(pesocajon,preciocajon));
+            alert (`el precio del kg de ${producto}  es $ ${reventa(pesoCajon, precioCajon)}`);
             break
              default:
                 alert ("ingrese un tipo valido de cliente");
@@ -37,5 +50,7 @@ do {
 
  }
 
-console.log (tipocliente);
-console.log(precioventa);
+
+console.log (tipoCliente);
+console.log(precioVenta);
+console.log(productos)
